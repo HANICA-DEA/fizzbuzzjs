c486422b0c6b9a9d27e0cd1d172d1f65e8689c45
Rody Middelkoop <rody.middelkoop@han.nl>
2019-12-12 15:11:16 +0100

    Switch from Chrome to headless no sandbox mode

--- karma.conf.js
+++ karma.conf.js
@@ -1,4 +1,6 @@
 const Path = require( "path" );
+const os = require('os');
+const chromeHeadlessSupported = os.platform() !== 'win32' || Number((os.release().match(/^(\d+)/) || ['0', '0'])[1]) >= 10;
 
 module.exports = function( config ) {
   config.set( {
@@ -15,7 +17,16 @@ module.exports = function( config ) {
 
     reporters: ["coverage-istanbul" ],
 
-    browsers: ["ChromeHeadless"],
+    browsers: [
+      chromeHeadlessSupported ? 'ChromeHeadless' : 'Chrome'
+    ],
+
+    customLaunchers: {
+      ChromeHeadless: {
+        base: 'Chrome',
+        flags: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222']
+      }
+    },
 
     singleRun: true,
 
